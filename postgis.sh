ba3e7b163eb357f04c637433db574bec4eda048a
Max Zhao <alcasa.mz@gmail.com>
2019-05-23 21:34:54 +0200

    Updating postgis sh to run a dev instance of postgis

--- postgis.sh
+++ postgis.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
-docker run -it -v $(pwd):/data postgis $@
+mkdir -p data
+docker run --env POSTGRES_PASSWORD=devtest --env POSTGRES_DB=geotest -it -v data:/var/lib/postgresql/data mdillon/postgis:11-alpine $@
