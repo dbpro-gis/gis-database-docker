43ba3aba5d2ce5b6dfa659a2a291de2b87a30a07
Max Zhao <alcasa.mz@gmail.com>
2019-06-02 00:38:43 +0200

    Adding missing environment variables

--- postgis_test.sh
+++ postgis_test.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 mkdir -p data
-docker run --env POSTGRES_PASSWORD=devtest --env POSTGRES_DB=geotest -it -v data:/var/lib/postgresql/data mdillon/postgis:11-alpine $@
+docker run --rm --name postgis-test --env POSTGRES_USER=postgres --env POSTGRES_PASSWORD=devtest --env POSTGRES_DB=geotest --env POSTGIS_ENABLE_OUTDB_RASTERS=1 --env POSTGIS_GDAL_ENABLED_DRIVERS=ENABLE_ALL -it -p 5432:5432 -v $(pwd)/data:/var/lib/postgresql/data mdillon/postgis:11-alpine $@
